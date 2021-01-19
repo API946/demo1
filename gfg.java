ed6c169d8406c7a6fe6be3f105df304b9c02a4b2
API964 <aditya.veer@apisero.com>
2021-01-19 17:36:56 +0530

    First Commit

---
+++ gfg.java
@@ -0,0 +1,13 @@
+
+class gfg { 
+	public static void main(String[] args) 
+	{ 
+		System.out.println("Welcome"); 
+		System.out.println("To"); 
+		System.out.println("GeeksforGeeks"); 
+
+		System.out.println("fffffffffff "); 
+		System.out.println("rrrrrrrrrr"); 
+		System.out.println("hhhhhhhhhhhhh"); 
+	} 
+} 
